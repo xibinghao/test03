76dac32f4012b43ed53db49aad5f734daa8194da
XiBingHao <2769855931@qq.com>
2020-04-01 00:03:49 +0800

    本地工程添加到git

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello Git!");
+        System.out.println("Hello Git!");
+        System.out.println("Hello Git!");
+        System.out.println("Hello Git!");
+        System.out.println("Hello Git!");
+        System.out.println("Hello Git!");
+        System.out.println("Hello Git!");
+        System.out.println("Hello Git!");
+        System.out.println("Hello Git!");
+        System.out.println("Hello Git!");
+        System.out.println("Hello Git!");
+    }
+}
